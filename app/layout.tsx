0044558df4b915715e4ebf91f15918703404b5c5
MANGAROLIYA UMANG <mangaroliyaumang21@gmail.com>
2026-01-24 19:26:35 +0530

    seo

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,11 +13,61 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata: Metadata = {
-  title: "Umang Portfolio",
-  description: "MERN stack Developer",
+  title: "Umang Magaroliya | Senior Software Developer Portfolio",
+  description:
+    "Senior Software Developer with 3+ years of experience in building scalable, responsive, and high-performance web applications using React.js, Next.js, TypeScript, and Tailwind CSS. Skilled in API integration, reusable component architecture, and performance optimization to deliver seamless user experiences",
   icons: {
     icon: "favicon.png",
   },
+  keywords: [
+    "Umang",
+    "Magaroliya",
+    "Umang Magaroliya",
+    "Senior Software Developer",
+    "Full Stack Developer",
+    "React Developer",
+    "Node.js Developer",
+    "Web Development",
+    "Portfolio",
+    "JavaScript",
+    "TypeScript",
+    "Tailwind CSS",
+    "Frontend Engineer",
+    "Redux",
+    "Next.js",
+  ],
+
+  authors: [{ name: "Umang Mangaroliya" }],
+  robots: "index, follow",
+  themeColor: "#e7000b",
+
+  openGraph: {
+    type: "website",
+    url: "https://portfolio-sigma-lilac-gzg2a5d2nq.vercel.app/",
+    title: "Umang | Senior Software Developer Portfolio",
+    description:
+      "Explore the tactical web operations of Umang. 3+ years of experience in building modern, high-performance web applications.",
+    images: [
+      {
+        url: "https://portfolio-sigma-lilac-gzg2a5d2nq.vercel.app/images/umang.jpg",
+        width: 1200,
+        height: 630,
+        alt: "Umang Portfolio Preview",
+      },
+    ],
+  },
+
+  twitter: {
+    card: "summary_large_image",
+    title: "Umang Magaroliya | Senior Software Developer Portfolio",
+    description:
+      "Explore the tactical web operations of Umang. 3+ years of experience in building modern, high-performance web applications.",
+    images: [
+      "https://portfolio-sigma-lilac-gzg2a5d2nq.vercel.app/images/umang.jpg",
+    ],
+  },
+
+  metadataBase: new URL("https://portfolio-sigma-lilac-gzg2a5d2nq.vercel.app/"),
 };
 
 export default function RootLayout({
